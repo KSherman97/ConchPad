57b097af5c2a11620367d421c5cc6267d1c74a48
Kyle Sherman <22063898+KSherman97@users.noreply.github.com>
2025-08-05 19:56:35 -0400

    added home, end, and del key

--- src/main.c
+++ src/main.c
@@ -33,7 +33,10 @@ enum editorKey {
   ARROW_UP = 3000,
   ARROW_DOWN = 4000,
   PAGE_UP,
-  PAGE_DOWN
+  PAGE_DOWN,
+  HOME_KEY,
+  END_KEY,
+  DEL_KEY
 };
 
 /** data **/
@@ -142,8 +145,13 @@ int editorReadKey() {
 
         if(seq[2] == '~') {
           switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
           }
         }
       } else {
@@ -152,10 +160,16 @@ int editorReadKey() {
           case 'B': return ARROW_DOWN;
           case 'C': return ARROW_RIGHT;
           case 'D': return ARROW_LEFT;
-        }
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
       }
     }
-
+  } else if (seq[0] == '0') {
+    switch (seq[1]) {
+      case 'H': return HOME_KEY;
+      case 'F': return END_KEY;
+    }
+  }
     return '\x1b';
   } else {
     return input;
@@ -342,6 +356,13 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3); 
       exit(0);
       break; 
+
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;
 
     case PAGE_UP:
     case PAGE_DOWN:
